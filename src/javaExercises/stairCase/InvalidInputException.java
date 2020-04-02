4f6100e929f648c2cce3de58fee5cf36e497c0bf
anciuvaga <andrei.ciuvaga@endava.com>
2020-04-02 13:09:37 +0300

    Adding exception handling Adding StringBuilder method, modify for with while

---
+++ src/javaExercises/stairCase/InvalidInputException.java
@@ -0,0 +1,7 @@
+package javaExercises.stairCase;
+
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String errorMessage){
+        super(errorMessage);
+    }
+}
---
+++ src/javaExercises/stairCase/StairCase.java
@@ -0,0 +1,35 @@
+package javaExercises.stairCase;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class StairCase {
+
+
+    public static void main(String[] args) throws InvalidInputException {
+        int numberOfStairs;
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Input desired staircase height:");
+            numberOfStairs = input.nextInt();
+        } catch(InputMismatchException e){
+            throw new InvalidInputException ("Wrong input");
+        }
+
+        System.out.print(returnStairCase(numberOfStairs));
+    }
+
+    private static StringBuilder returnStairCase(int numberOfStairs) {
+        StringBuilder stairs = new StringBuilder("");
+        for (int i = 1; i <= numberOfStairs; i++) {
+            int j = 1;
+            while (j <= numberOfStairs) {
+                if (i + j > numberOfStairs)
+                    stairs.append("#");j++;
+            }
+           stairs.append("\n");
+        }
+        return stairs;
+    }
+
+}
