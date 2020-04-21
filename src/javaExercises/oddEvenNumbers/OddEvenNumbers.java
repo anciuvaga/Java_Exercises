752f83006a409facd601085ea45994ae59feae6f
anciuvaga <andrei.ciuvaga@endava.com>
2020-04-22 01:15:34 +0300

    Adding lambda expression to calculate even and odd numbers

--- src/javaExercises/oddEvenNumbers/OddEvenNumbers.java
+++ src/javaExercises/oddEvenNumbers/OddEvenNumbers.java
@@ -3,37 +3,14 @@ package javaExercises.oddEvenNumbers;
 import java.util.Arrays;
 
 public class OddEvenNumbers {
-
-    public static int calcEvenNumbers(int[] arr){
-       int amountEvenNumbers = 0;
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i] % 2 == 0)
-                amountEvenNumbers++;
-        }
-        return amountEvenNumbers;
-    }
-
-    public static int calcOddNumbers(int[] arr){
-        int sumOfOddNumbers = 0;
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i] % 2 != 0)
-                sumOfOddNumbers += arr[i];
-        }
-        return sumOfOddNumbers;
-    }
-
     public static void findNumbers(int[] arr) {
-
         System.out.println("Elements of the array are: " + Arrays.toString(arr));
-        System.out.println("In arrey are: " + calcEvenNumbers(arr) + " even numbers\nSum of odd numbers are: " + calcOddNumbers(arr));
+        System.out.println("In arrey are: " + Arrays.stream(arr).filter(x -> x % 2 == 0).count()
+                + " even numbers\nSum of odd numbers are: " + Arrays.stream(arr).filter(x -> x % 2 != 0).sum());
     }
 
     public static void main(String[] args) {
-
         int[] arr = {1, 6, 12, 8, 9};
-
         findNumbers(arr);
     }
 }
