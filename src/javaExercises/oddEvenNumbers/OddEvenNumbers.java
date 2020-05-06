f8141b6a5fb0fe7f95f80d0ff474a95575ddf412
anciuvaga <andrei.ciuvaga@endava.com>
2020-05-06 14:19:08 +0300

    Split implementation into 2 methods

--- src/javaExercises/oddEvenNumbers/OddEvenNumbers.java
+++ src/javaExercises/oddEvenNumbers/OddEvenNumbers.java
@@ -3,14 +3,29 @@ package javaExercises.oddEvenNumbers;
 import java.util.Arrays;
 
 public class OddEvenNumbers {
-    public static void findNumbers(int[] arr) {
-        System.out.println("Elements of the array are: " + Arrays.toString(arr));
-        System.out.println("In arrey are: " + Arrays.stream(arr).filter(x -> x % 2 == 0).count()
-                + " even numbers\nSum of odd numbers are: " + Arrays.stream(arr).filter(x -> x % 2 != 0).sum());
+
+    public static int calcEvenNumbers(int[] arr) {
+        return (int) Arrays.stream(arr).filter(x -> x % 2 == 0).count();
+    }
+
+    public static int calcOldNumbers(int[] arr) {
+        return Arrays.stream(arr).filter(x -> x % 2 != 0).sum();
+    }
+
+    public static void printArray(int[] arr) {
+        System.out.println("Array elements are:");
+        Arrays.stream(arr)
+                .forEach(element -> System.out.print(element + " "));
+        System.out.println();
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 6, 12, 8, 9};
-        findNumbers(arr);
+
+        printArray(arr);
+        System.out.println("Counted even numbers:");
+        System.out.println(calcEvenNumbers(arr));
+        System.out.println("Sum of odd numbers:");
+        System.out.println(calcOldNumbers(arr));
     }
 }
