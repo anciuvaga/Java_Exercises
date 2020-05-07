d1ce0cf0af09d926b72112ad95f7815916c62baf
anciuvaga <andrei.ciuvaga@endava.com>
2020-05-07 13:10:47 +0300

    Adding custom exception handling

--- src/javaExercises/primeNumbers/PrimeNumbers.java
+++ src/javaExercises/primeNumbers/PrimeNumbers.java
@@ -3,11 +3,11 @@ package javaExercises.primeNumbers;
 import java.util.Scanner;
 
 public class PrimeNumbers {
-    public static boolean isPrime(int n) {
-        if (n == 1)
-            return false;
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0)
+    public static boolean isPrime(int potentialPrimeNumber) {
+
+        for (int i = 2; i < potentialPrimeNumber; i++) {
+
+            if (potentialPrimeNumber % i == 0)
                 return false;
         }
         return true;
@@ -16,19 +16,21 @@ public class PrimeNumbers {
     public static void printPrimeNumbers(int n) {
         for (int i = 2; i < n; i++) {
             if (isPrime(i))
-                System.out.print(+i + ", ");
+                System.out.print(i + ", ");
         }
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NotPrimeNumberExeption {
+        int potentialPrimeNumber;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter potential prime number: ");
+        potentialPrimeNumber = input.nextInt();
+        if (potentialPrimeNumber == 0 || potentialPrimeNumber == 1)
+            throw new NotPrimeNumberExeption("Sorry, entered number: "
+                    + potentialPrimeNumber + " is not a prime number");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number n: ");
-        int n = sc.nextInt();
-
-        printPrimeNumbers(n);
-
+        printPrimeNumbers(potentialPrimeNumber);
     }
 }
 
